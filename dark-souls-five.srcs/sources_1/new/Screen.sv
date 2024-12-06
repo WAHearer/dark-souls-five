138832f62be8b28e9877a8c73c241aeb4b970f55
IzakyL <971918378@qq.com>
2024-12-06 22:29:49 +0800

    fix: 双缓冲

--- dark-souls-five.srcs/sources_1/new/Screen.sv
+++ dark-souls-five.srcs/sources_1/new/Screen.sv
@@ -26,19 +26,31 @@ module Screen #(
     output reg vga_hs,           // 行同步
     output reg vga_vs            // 场同步
 );
- 
+reg buffer_select;
+reg buffer_swap_ready;
 // VRAM接口
-reg  vram_we;
-reg  [11:0] vram_din;
-wire [11:0] vram_dout;
-blk_mem_gen_0 vram (
+reg  [11:0] vram_din_a, vram_din_b;
+wire [11:0] vram_dout_a, vram_dout_b;
+wire [11:0] display_data = buffer_select ? vram_dout_b : vram_dout_a;
+reg vram_we_a, vram_we_b;
+blk_mem_gen_0 vram_A (
     .clka(clk_50),
-    .wea(vram_we),
+    .wea(vram_we_a),
     .addra({8'hff - render_y, 8'hff - render_x}),
-    .dina(vram_din),
+    .dina(vram_din_a),
     .clkb(clk),
     .addrb({y, x}),
-    .doutb(vram_dout)
+    .doutb(vram_dout_a)
+);
+
+blk_mem_gen_0 vram_B (
+    .clka(clk_50),
+    .wea(vram_we_b),
+    .addra({8'hff - render_y, 8'hff - render_x}),
+    .dina(vram_din_b),
+    .clkb(clk),
+    .addrb({y, x}),
+    .doutb(vram_dout_b)
 );
 
 // 渲染逻辑
@@ -64,10 +76,15 @@ always @(posedge clk_50) begin
     case (render_state)
         IDLE: begin
             render_state <= RENDER_ENEMY;
-            vram_we <= 1;
             render_x <= enemyPosition[0] - 7'h7;
             render_y <= enemyPosition[1] - 7'h7;
-            vram_din <= COLOR_ENEMY;
+            if (buffer_select) begin
+                vram_we_a <= 1;
+                vram_din_a <= COLOR_ENEMY;
+            end else begin
+                vram_we_b <= 1;
+                vram_din_b <= COLOR_ENEMY;
+            end
         end
 
         RENDER_ENEMY: begin
@@ -77,7 +94,13 @@ always @(posedge clk_50) begin
                     render_state <= RENDER_PLAYER;
                     render_x <= playerPosition[0] - 7'h7;
                     render_y <= playerPosition[1] - 7'h7;
-                    vram_din <= COLOR_PLAYER;
+                    if (buffer_select) begin
+                        vram_we_a <= 1;
+                        vram_din_a <= COLOR_PLAYER;
+                    end else begin
+                        vram_we_b <= 1;
+                        vram_din_b <= COLOR_PLAYER;
+                    end
                 end else begin
                     render_y <= render_y + 1;
                 end
@@ -93,7 +116,13 @@ always @(posedge clk_50) begin
                     render_state <= RENDER_PLAYER_BULLET;
                     render_x <= playerBullet[0];
                     render_y <= playerBullet[1];
-                    vram_din <= COLOR_PLAYER_BULLET;
+                    if (buffer_select) begin
+                        vram_we_a <= 1;
+                        vram_din_a <= COLOR_PLAYER_BULLET;
+                    end else begin
+                        vram_we_b <= 1;
+                        vram_din_b <= COLOR_PLAYER_BULLET;
+                    end
                     bulletCounter <= 0;
                 end else begin
                     render_y <= render_y + 1;
@@ -108,7 +137,13 @@ always @(posedge clk_50) begin
                 render_state <= RENDER_ENEMY_BULLET;
                 render_x <= enemyBullet[0];
                 render_y <= enemyBullet[1];
-                vram_din <= COLOR_ENEMY_BULLET;
+                if (buffer_select) begin
+                    vram_we_a <= 1;
+                    vram_din_a <= COLOR_ENEMY_BULLET;
+                end else begin
+                    vram_we_b <= 1;
+                    vram_din_b <= COLOR_ENEMY_BULLET;
+                end
                 bulletCounter <= 0;
             end else begin
                 bulletCounter <= bulletCounter + 1;
@@ -128,8 +163,13 @@ always @(posedge clk_50) begin
         end
 
         DONE: begin
-            vram_we <= 0;
-            render_state <= IDLE;
+            if (buffer_select) begin
+                vram_we_a <= 0;
+            end else begin
+                vram_we_b <= 0;
+            end
+            buffer_swap_ready <= 1; 
+            render_state <= buffer_swap_ready ? DONE : IDLE;
         end
     endcase
  end
@@ -140,36 +180,40 @@ initial begin
     hcount = 0;
     vcount = 0;
 end
- always @(posedge clk_50) begin
-     if(hcount == HSW + BP + HEN + HFP - 1) begin
+always @(posedge clk_50) begin
+    if(hcount == HSW + BP + HEN + HFP - 1) begin
         hcount <= 0;
-         if(vcount == VSW + VBP + VEN + VFP - 1)
-             vcount <= 0;
-         else
+        if(vcount == VSW + VBP + VEN + VFP - 1) begin
+            vcount <= 0;
+            if (buffer_swap_ready) begin
+                buffer_select <= ~buffer_select;
+                buffer_swap_ready <= 0;
+            end
+        end else
              vcount <= vcount + 1;
-     end
+    end
     else
         hcount <= hcount + 1;
- end
+end
  
 // 同步信号生成
- always @(posedge clk_50) begin
+always @(posedge clk_50) begin
     vga_hs <= (hcount < HSW);
     vga_vs <= (vcount < VSW);
- end
+end
  
 // 显示使能信号
 wire disp_enable = (hcount >= HSW + BP) && (hcount < HSW + BP + HEN) &&
-                  (vcount >= VSW + VBP) && (vcount < VSW + VBP + VEN);
+                   (vcount >= VSW + VBP) && (vcount < VSW + VBP + VEN);
 
 // 显示坐标计算
 wire [7:0] x, y;
 assign x = (hcount - (HSW + BP)) >> 2;
 assign y = (vcount - (VSW + VBP)) >> 2;
 
-assign vga_r = disp_enable ? vram_dout[11:8] : 4'h0;
-assign vga_g = disp_enable ? vram_dout[7:4] : 4'h0;
-assign vga_b = disp_enable ? vram_dout[3:0] : 4'h0;
+assign vga_r = disp_enable ? display_data[11:8] : 4'h0;
+assign vga_g = disp_enable ? display_data[7:4] : 4'h0;
+assign vga_b = disp_enable ? display_data[3:0] : 4'h0;
 
 
 endmodule
