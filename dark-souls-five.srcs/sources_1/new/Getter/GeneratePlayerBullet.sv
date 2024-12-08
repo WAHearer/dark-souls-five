1ed282e61d7f86c4e807b854592d161ef2094877
Tianhe Zhang <62363213+WAHearer@users.noreply.github.com>
2024-12-08 15:15:21 +0800

    Add files via upload

--- dark-souls-five.srcs/sources_1/new/Getter/GeneratePlayerBullet.sv
+++ dark-souls-five.srcs/sources_1/new/Getter/GeneratePlayerBullet.sv
@@ -6,40 +6,55 @@ module GeneratePlayerBullet (
 
     output reg [27:0] next_playerBullet[0:39]
 );
-integer counter,startPos;
+integer counter;
 integer i;
 initial begin
     counter<=0;
-    startPos<=0;
     for(i=0;i<40;i++)
         next_playerBullet[i]<=0;
 end
 always @(posedge clk) begin
     if(state==2) begin
-        if(counter<32'd20000000)
-            counter<=counter+1;
+        if(counter<32'd160000000)
+            counter++;
         else
             counter<=0;
-        if(counter==0) begin
-            startPos<=(startPos+3)<40?(startPos+3):(startPos+3)-40;
-            next_playerBullet[(startPos+3)<40?(startPos+3):(startPos+3)-40]<={12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8);
-            next_playerBullet[(startPos+4)<40?(startPos+4):(startPos+4)-40]<={12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8);
-            next_playerBullet[(startPos+5)<40?(startPos+5):(startPos+5)-40]<={12'b100110010100,playerPosition[1],playerPosition[0]}+3+(1<<8);
-        end
-        else if(counter==1) begin
-            next_playerBullet[startPos]<=0;
-            next_playerBullet[(startPos+1)<40?(startPos+1):(startPos+1)-40]<=0;
-            next_playerBullet[(startPos+2)<40?(startPos+2):(startPos+2)-40]<=0;
-            next_playerBullet[(startPos+3)<40?(startPos+3):(startPos+3)-40]<=0;
-        end
+    end
+    else
+        counter<=0;
+end
+always @(*) begin
+    if(state==1||state==2) begin
+        next_playerBullet[0]=(counter==32'd20000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[1]=(counter==32'd20000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[2]=(counter==32'd20000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[3]=(counter==32'd40000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[4]=(counter==32'd40000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[5]=(counter==32'd40000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[6]=(counter==32'd60000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[7]=(counter==32'd60000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[8]=(counter==32'd60000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[9]=(counter==32'd80000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[10]=(counter==32'd80000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[11]=(counter==32'd80000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[12]=(counter==32'd100000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[13]=(counter==32'd100000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[14]=(counter==32'd100000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[15]=(counter==32'd10000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[16]=(counter==32'd120000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[17]=(counter==32'd120000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[18]=(counter==32'd140000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[19]=(counter==32'd140000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[20]=(counter==32'd140000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        next_playerBullet[21]=(counter==32'd160000000)?{12'b100110010100,playerPosition[1],playerPosition[0]}+(1<<8):0;
+        next_playerBullet[22]=(counter==32'd160000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}-3+(1<<8):0;
+        next_playerBullet[23]=(counter==32'd160000000)?{12'b100110001010,playerPosition[1],playerPosition[0]}+3+(1<<8):0;
+        for(i=24;i<40;i++)
+            next_playerBullet[i]=0;
     end
     else begin
-        counter<=0;
-        if(state!=1) begin
-            startPos<=0;
-            for(i=0;i<40;i++)
-                next_playerBullet[i]<=0;
-        end
+        for(i=0;i<40;i++)
+            next_playerBullet[i]=0;
     end
 end
 endmodule
