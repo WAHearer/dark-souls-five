92291f189a93248f9d9d46f1875cea8c42a1e9dd
IzakyL <971918378@qq.com>
2024-12-26 18:13:14 +0800

    fix

--- dark-souls-five.srcs/sources_1/new/Screen.sv
+++ dark-souls-five.srcs/sources_1/new/Screen.sv
@@ -34,18 +34,14 @@ reg render_ready, vga_ready;
 reg  [11:0] vram_din_a, vram_din_b;
 wire [11:0] vram_dout_a, vram_dout_b;
 wire rom_text_out;
-reg [11:0] rom_figure_offset_x, rom_figure_offset_y;
-wire [7:0] rom_figure_in_y, rom_figure_in_x;
-assign rom_figure_in_x = render_x - rom_figure_offset_x;
-assign rom_figure_in_y = render_y - rom_figure_offset_y;
+reg [7:0] rom_figure_in_y, rom_figure_in_x;
 wire [11:0] rom_figure_out;
 reg [3:0] imgID;
 reg [7:0] img_y, img_x;
 wire [11:0] display_data = buffer_select ? vram_dout_b : vram_dout_a;
 reg vram_we_a, vram_we_b;
 reg [11:0] hcount, vcount;
-wire figID;
-assign figID = level - 1;
+reg figID;
 initial begin
     hcount = 0;
     vcount = 0;
@@ -66,6 +62,14 @@ always @(posedge clk) begin
         3:disp_hp <= enemyHp >> 4;
         4:disp_hp <= enemyHp >> 4;
         5:disp_hp <= enemyHp >> 5;
+    endcase
+
+    case (level)
+        1:figID <= 0;
+        2:figID <= 1;
+        3:figID <= 2;
+        4:figID <= 3;
+        5:figID <= 4; 
     endcase
 end
 blk_mem_gen_0 vram_A (
@@ -212,8 +216,8 @@ always @(posedge clk) begin
                     render_state <= RENDER_ENEMY;
                     render_x <= enemyPosition[0] - 20;
                     render_y <= enemyPosition[1] - 20;
-                    rom_figure_offset_x <= enemyPosition[0] - 20;
-                    rom_figure_offset_y <= enemyPosition[1] - 20;
+                    rom_figure_in_x <= 0;
+                    rom_figure_in_y <= 0;
                     if (buffer_select) begin
                         vram_we_a <= 1;
                         vram_din_a <= rom_figure_out;
@@ -232,6 +236,7 @@ always @(posedge clk) begin
         RENDER_ENEMY: begin
             if (render_x == (enemyPosition[0] + 20) || render_x == 199) begin
                 render_x <= enemyPosition[0] - 20;
+                rom_figure_in_x <= 0;
                 if (render_y == (enemyPosition[1] + 20) || render_y == 149) begin
                     render_state <= RENDER_PLAYER;
                     render_x <= playerPosition[0];
@@ -245,9 +250,11 @@ always @(posedge clk) begin
                     end
                 end else begin
                     render_y <= render_y + 1;
+                    rom_figure_in_y <= rom_figure_in_y + 1;
                 end
             end else begin
                 render_x <= render_x + 1;
+                rom_figure_in_x <= rom_figure_in_x + 1;
             end
         end
 
